00f09085d0da376169b6ac198874662eaac1f5c9
Luciano de Oliveira <luciano.de.oliveira.2822@gmail.com>
2026-09-04 13:17:24 -0300

    Correção
    pastas com capitalização dão problema com o git ignore

--- aulas/codigos_por_unidade/unidade2/modulo2/Aula.java
+++ aulas/codigos_por_unidade/unidade2/modulo2/Aula.java
@@ -7,20 +7,19 @@ public class Aula {
 
 
         /*
-        operadores de atribuição
+        Operadores de atribuição
         = atribuição numero = 3;
         += atribuição com soma numero += 1; == 2
         -= atribuição com subtração numero -= 1; == 1
         *= atribuição com multiplicação
         /= atribuição com divisão
         %= atribuição com módulo número %= 2; == 0 3/2 = 1 ()
-        */
 
-        // declaração da variavel opeAtribuicao do tipo integer/int com 0 como valor inicial atribuido
+        // declaração da variável opeAtribuicao do tipo integer/int com 0 como valor inicial atribuído
         int opeAtribuicao = 0;
         System.out.println(opeAtribuicao);
 
-        // atribuição com soma, nessa variavel ja temos o valor de 0 e a ele sera somado 3, então 0 + 3 = 3
+        // atribuição com soma, nessa variável ja temos o valor de 0 e a ele será somado 3, então 0 + 3 = 3
         opeAtribuicao += 3;
         System.out.println(opeAtribuicao);
 
@@ -36,15 +35,13 @@ public class Aula {
         opeAtribuicao /= 2;
         System.out.println(opeAtribuicao);
 
-        // atribuição com modulo ou resto de divisão, por conta da operação anterior temos 2, então 2 / 2 = 1 (0)
+        // atribuição com módulo ou resto de divisão, por conta da operação anterior temos 2, então 2 / 2 = 1 (0)
         //  2 / 2    3 / 2    17 / 2     10 / 3         10 = 3 * 3 + 1
         // (0)  1   (1)  1   (1)   8     (1)  3,333...
         opeAtribuicao %= 2;
         System.out.println(opeAtribuicao);
 
 
-        //
-        /*
         Operadores relacionais
         == "igual a"
         != "diferente de"
@@ -83,8 +80,6 @@ public class Aula {
             System.out.println("Somente se ele for 1");
         }
 
-
-
         operadores aritméticos
         + "soma"
         - "subtração"
@@ -94,10 +89,36 @@ public class Aula {
         ++ "incremento" numero = 1; numero++;
         -- "decremento" numero--;
 
+        // declaração da variável opeAritmeticos do tipo integer/int com a atribuição do valor 0
+        int opeAritmeticos = 0;
+
+        // 2 * 3 == 6
+        // 75 / 5 == 15
+        // 35 % 5 == 0
+        // 6 + 15 - 0 == 21
+        opeAritmeticos = (2 * 3) + (75 / 5) - (35 % 5);
+        System.out.println(opeAritmeticos);
+
+        // soma por 1
+        opeAritmeticos++;
+        System.out.println(opeAritmeticos);
+
+        // subtrai por 1
+        opeAritmeticos--;
+        System.out.println(opeAritmeticos);
+
         Operadores lógicos
         && "e"
         || "ou"
         ! "Não"
+
+        int opeLogicos = 0;
+
+        boolean opeLogi = opeLogicos == 0
+
+        if(opeLogicos == 1 && !opeLogi == false || opeLogicos == 75){
+            System.out.println("Ira imprimir 1");
+        }
 
         condicionais
         if(){}
@@ -107,6 +128,29 @@ public class Aula {
 
         em expressões complexas o interpretador ira ler primeiro "()", depois "!",
         depois os símbolos relacionais, depois "&&" e por último "||"
+
+        // declaração da variavel expComplexa do tipo integer/int com a atribuição de valor 10
+        int expComplexa = 10;
+
+        // declaração da variavel ativo do tipo booleana/boolean com a atribuição do valor true/verdadeiro
+        boolean ativo = true;
+
+        // Primeiro o entre parênteses mais interno
+        // expComplexa < 20 == verdadeiro
+        // segundo a outra opção separada pelo ||
+        // ativo originalmente é true, mas aqui ele esta sendo negado, então ele é false, logo
+        // !ativo == false
+        // então um retorna true e o outro false, como estão separadas pela logica de ou, retornam true
+        // o parênteses mais interno é igual a () == true
+        // então por enquanto temos !(? && true);
+        // expComplexa > 5 == true
+        // então !(true && true) == true
+        // e de acordo com a tabela verdade true && true == true, logo () == true
+        // mas temos mais uma negação, então !() == false
+        boolean resultado = !(expComplexa > 5 && (expComplexa < 20 || !ativo));
+
+        System.out.println(resultado);
+
         */
 
         // declaração de variáveis
